61da33fafc496276d2e1b7ac382f22a0246030f3
peguesj <jeremiah.pegues@gmail.com>
2026-04-28 09:37:01 -0400

    fix: correct LinkedIn URL to asklilyhealth

--- src/components/SiteFooter.tsx
+++ src/components/SiteFooter.tsx
@@ -35,7 +35,7 @@ const COLUMNS = [
   {
     heading: 'Connect',
     links: [
-      { label: 'LinkedIn', href: 'https://www.linkedin.com/company/asklily' },
+      { label: 'LinkedIn', href: 'https://www.linkedin.com/company/asklilyhealth/' },
       { label: 'Instagram', href: 'https://www.instagram.com/asklilyhealth' },
       { label: 'Twitter / X', href: 'https://x.com/asklilyhealth' },
       { label: 'Facebook', href: 'https://www.facebook.com/profile.php?id=61588828805568' },
